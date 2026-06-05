b0a9b6bf351ae0a4d5812980cd6a3df4d2dbc726
ambokileB <wholemike2703email.com>
2026-06-06 00:12:07 +0300

    feat:fixing backend

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',   // local dev
   'http://localhost:5174',
-  'http://192.168.213.184:5173/',
+  'http://109.123.246.231:5173/',
   'https://www.vatesbet.com/',
   'http://www.betnover.com'
 ];
